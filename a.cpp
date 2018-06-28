0338718c7c817b122251bca94388fe77def50678
Volodya-Hovhannisyan <volodyahovhannisyan98@gmail.com>
2018-06-28 19:13:02 +0400

    Fourth commit

--- a.cpp
+++ a.cpp
@@ -2,17 +2,7 @@
 
 int main()
 {
-	std::cout << "jrjg" << std::endl;
 	
-	if(true)
-	{
-		std::cout << "second commit" << std::endl;
-	}
-
-
-	if(Ijfirjef) kpjfjef
-		
-
 
 
 	for(int i(0); i < n; ++i)
